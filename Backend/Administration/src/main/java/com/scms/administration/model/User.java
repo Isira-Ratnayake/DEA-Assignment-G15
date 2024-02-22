62d756f53e253afdad9516614d0f44bf3de05c20
Rushmore-Systems-PL <isira.rushmoresystems@gmail.com>
2024-02-22 13:22:29 +0530

    Models converted to entities.

--- Backend/Administration/src/main/java/com/scms/administration/model/User.java
+++ Backend/Administration/src/main/java/com/scms/administration/model/User.java
@@ -1,4 +1,27 @@
 package com.scms.administration.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userId;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id")
+    private Role role;
+
 }
